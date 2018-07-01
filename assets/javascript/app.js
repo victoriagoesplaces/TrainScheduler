784c73cddda799813342af128b8fcdece032ecd9
Victoria Gonzalez <vmjgr@Victorias-MBP.attlocal.net>
2018-06-30 20:04:36 -0400

    folder setup/css

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyADePX2QZfOnKm_bRxbPJdBMu035I_2Ec8",
+authDomain: "homework1-e6b68.firebaseapp.com",
+databaseURL: "https://homework1-e6b68.firebaseio.com",
+projectId: "homework1-e6b68",
+storageBucket: "",
+messagingSenderId: "649587614970"
+};
+
+firebase.initializeApp(config);
+
+  // variable to reference the database.
+var database = firebase.database();
