e284ba3233d166a9734149a59fbc1631ea880bd7
Victoria Gonzalez <vmjgr@Victorias-MBP.attlocal.net>
2018-07-02 22:17:48 -0400

    fuctional train information/database connection

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -65,26 +65,22 @@ database.ref().on("child_added", function(childSnapshot) {
   // console.log(frequency);
 
   // Calculate next arrival
+    var firstTrain = 0;
     // First Time (pushed back 1 year to make sure it comes before current time)
     var firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
     // console.log(firstTimeConverted);
-
     // Current Time
     var currentTime = moment();
     // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
     // Difference between the times
     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
     // console.log("DIFFERENCE IN TIME: " + diffTime);
-
     // Time apart (remainder)
     var tRemainder = diffTime % frequency;
     // console.log(tRemainder);
-
     // Minute Until Train
     var tMinutesTillTrain = frequency - tRemainder;
-    // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
     // Next Train
     var nextTrain = moment().add(tMinutesTillTrain, "minutes");
     // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
